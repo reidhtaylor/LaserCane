396a764d9a7daeb9957d79da3fb8862ff90ce7d7
Reid Taylor <reidhtaylor@icloud.com>
2025-10-23 19:08:49 -0400

    Added working sleep mode and prepped for long press and button debouncing

--- blink.py
+++ blink.py
@@ -17,13 +17,20 @@ LED_PIN = Pin(25, Pin.OUT)
 ACTIVATE_BTN_PIN_INDEX = 4
 ACTIVATE_BTN_PIN = Pin(ACTIVATE_BTN_PIN_INDEX, Pin.IN, Pin.PULL_DOWN) # Pin labeled 4 on Pico is PIN_3
 
-NORMAL_MC_FREQ = 133_000_000
-DEEP_SLEEP_MC_FREQ = 12_000_000
+NORMAL_MC_FREQ = 125_000_000
+DEEP_SLEEP_MC_FREQ = 48_000_000
 
 MEASUREMENTS_PER_SECOND = 100 # Measurements taken per second
 
 LUNA_SLAVE_ADDRESS = 0x10 # Slave address of I2C frequency
-LUNA_SEND_DATA = bytearray({ 0x5A, 0x05, 0x00, 0x01, 0x60 }) # Default I2C Send data
+LUNA_SEND_DATA = bytearray([ 0x5A, 0x05, 0x00, 0x01, 0x60 ]) # Default I2C Send data
+
+class SYSTEM_STATE:
+    NONE = -1
+    SLEEPING = 0
+    ACTIVE = 1
+    
+system_state = SYSTEM_STATE.NONE
 
 # -––––––––––––––––––––––––––––––––––––––––––––––– I2C
 i2c_handler = None
@@ -45,40 +52,57 @@ def deinit_i2c():
 
 # -––––––––––––––––––––––––––––––––––––––––––––––– Luna Sensor
 def power_sensor(turn_on: bool):
+    if not i2c_handler: return
+    
     if turn_on:
         print("Luna Sensor Activated")
     else:
         print("Luna Sensor Deactivated")
+
+    enable_hex = 0x01 if turn_on else 0x00
+    freq = 0x01 if turn_on else 0x00
+    cs = 0x91 if turn_on else 0x8F
+    byte_cmd = bytearray([0x5A, 0x35, enable_hex, freq, 0x00, 0x00, 0x00, cs])
+    
+    i2c_handler.writeto(LUNA_SLAVE_ADDRESS, byte_cmd)
 # -––––––––––––––––––––––––––––––––––––––––––––––– Luna Sensor
 
 # -––––––––––––––––––––––––––––––––––––––––––––––– Activate Button
 def _noop_irq(pin):
-    # breaks the deep sleep of MC (called by button)
+    # breaks the lightsleep of MC (called by button)
     pass
 
 ACTIVATE_BTN_PIN.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=_noop_irq)
 
-def activate_btn_pressed():
-    # global ACTIVATE_BTN_PIN
+# Checks for valid activation trigger
+def wait_for_activate():
     return ACTIVATE_BTN_PIN.value() == 1
+
+# Checks for valid deactivation trigger
+def wait_for_deactivate():
+    return ACTIVATE_BTN_PIN.value() == 0
 # -––––––––––––––––––––––––––––––––––––––––––––––– Activate Button
 
 # -––––––––––––––––––––––––––––––––––––––––––––––– Power ON
 def run_on_cycle():
     global i2c_handler
-    global power_is_on
+    global system_state
     global ACTIVATE_BTN_PIN
     global LED_PIN
     
     init_i2c()
     power_sensor(True)
-    power_is_on = True
+    system_state = SYSTEM_STATE.ACTIVE
     LED_PIN.on()
-    print("\n--------------\nButton Released")
+    print("\n--------------\nButton Pressed")
     
     try:
-        while activate_btn_pressed():
-            if not i2c_handler:
+        while True:
+            if not i2c_handler: break
+            
+            # button released
+            result = wait_for_deactivate()
+            if result:
                 break
             
             i2c_handler.writeto(LUNA_SLAVE_ADDRESS, LUNA_SEND_DATA)
@@ -96,13 +120,14 @@ def run_on_cycle():
             temperature = (data[6] + data[7] * 256) / 8.0 - 256 # convert to F
             # 47 is avg and safe
             #
-            print(f"\rDistance: {distance}, Signal: {sig_strength}, Temp: {temperature}", end="")
+            print(f"Distance: {distance}, Signal: {sig_strength}, Temp: {temperature}", end="\n")
             
             sleep(1 / float(MEASUREMENTS_PER_SECOND)) # sleep 1sec
     finally:
-        deinit_i2c()
+        print("")
         power_sensor(False)
-        power_is_on = False
+        deinit_i2c()
+        system_state = SYSTEM_STATE.NONE
         LED_PIN.off()
         print("Button Released\n--------------\n")
     
@@ -110,38 +135,42 @@ def run_on_cycle():
 
 # -––––––––––––––––––––––––––––––––––––––––––––––– Power OFF
 def idle_sleep_forever():
-    try:
-        freq(DEEP_SLEEP_MC_FREQ)
-    except Exception:
-        pass
+    global system_state
+    
+    if system_state == SYSTEM_STATE.SLEEPING: return
+    system_state = SYSTEM_STATE.SLEEPING
+    
+    freq(DEEP_SLEEP_MC_FREQ)
     
     # put device into sleep mode
-    lightsleep()  # interrupted by IRQ from button
+    print("\rChecking for sleep wakeup...", end="\n")
+    lightsleep(60_000)  # interrupted by IRQ from button
     
     # sleep was interrupted -> button pressed
-    try:
-        freq(NORMAL_MC_FREQ)
-    except Exception:
-        pass
+    freq(NORMAL_MC_FREQ)
+    
+    system_state = SYSTEM_STATE.NONE
 # -––––––––––––––––––––––––––––––––––––––––––––––– Power OFF
 
 # -––––––––––––––––––––––––––––––––––––––––––––––– PROGRAM
-power_is_on = False
 def main():
-    global power_is_on
+    global system_state
     global LED_PIN
     
     # Start idled
+    init_i2c()
     power_sensor(False)
     deinit_i2c()
     LED_PIN.off()
     try:
         while True:
             # deep idle sleep
-            # TODO - Include this sleep function
-            # idle_sleep_forever()
+            idle_sleep_forever()
             
-            if activate_btn_pressed(): # button pressed -> activate
+            valid_press = wait_for_activate()
+            if valid_press:
+                # press started
+                
                 # stalls code until press stopped
                 run_on_cycle()
                 
